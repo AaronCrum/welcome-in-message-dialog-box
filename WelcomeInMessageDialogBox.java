dc9e3d6131f3d714b105305714a98ff42ad655b1
Aaron <59898602+AaronCrum@users.noreply.github.com>
2021-03-08 14:57:07 -0800

    Add files via upload

---
+++ WelcomeInMessageDialogBox.java
@@ -0,0 +1,10 @@
+//** This application displays Welcome to Java! * in a message dialog box. * *//
+
+import java.x.swing.JOptionPane;
+
+public class WelcomeInMessageDialogBox {
+   public static void main (String [] args) {
+      // Display Welcome to Java! in a message dialog box
+      JOptionPane.showMessageDialog(null, "Happy Halloween!", "Holiday Greetings");
+         }
+       }
