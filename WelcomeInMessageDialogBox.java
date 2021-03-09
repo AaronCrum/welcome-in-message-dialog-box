842d6f0975b7ae02e4f623abf090ffdb3a949ad8
Aaron <59898602+AaronCrum@users.noreply.github.com>
2021-03-08 22:02:53 -0800

    Update WelcomeInMessageDialogBox.java

--- WelcomeInMessageDialogBox.java
+++ WelcomeInMessageDialogBox.java
@@ -7,5 +7,5 @@ import java.x.swing.JOptionPane;
 public class WelcomeInMessageDialogBox {
    public static void main (String [] args) {
       JOptionPane.showMessageDialog(null, "Welcome to Java!");
-         }
-       }
+   }
+}
